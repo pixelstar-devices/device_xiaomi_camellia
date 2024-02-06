482ffdd5e3b19883c2779f89b853fb2eeec40be9
Noah113-Allen <kingfighter2958@gmail.com>
2024-02-06 09:14:58 +0530

    camellia: PixelStar bring-up
    Change-Id: I0a901ce694befcc0fcebd4e0fc2bf4a7dfe7779a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_camellia.mk
+    $(LOCAL_DIR)/pixelstar_camellia.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_camellia-user \
-    aosp_camellia-userdebug \
-    aosp_camellia-eng
+    pixelstar_camellia-user \
+    pixelstar_camellia-userdebug \
+    pixelstar_camellia-eng
